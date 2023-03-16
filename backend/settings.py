0fc6ce2d31d167a7489d145ec34afcff3621d77f
Arvind-4 <lordarvindthegamer@gmail.com>
2023-03-16 23:49:53 +0530

    Updated: static root bug

--- backend/settings.py
+++ backend/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / "static"
+
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
